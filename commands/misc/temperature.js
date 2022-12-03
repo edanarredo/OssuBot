cda17efd5682153a2967c9c9c441e6d44b0fdadd
Daniel <edanielarredondo@gmail.com>
2022-12-03 09:03:41 -0600

    Add comments

--- commands/misc/temperature.js
+++ commands/misc/temperature.js
@@ -10,16 +10,17 @@
  */
  module.exports = {
 	name: "temp",
-	// Refer to typings.d.ts for available properties.
 
 	execute(message, args) {
 
+      // Parse values from command argument
       const inputTemperature = args[0];
       const temperatureValue = parseInt(inputTemperature, 10);
       const temperatureType = inputTemperature.match(/[a-zA-Z]/)[0].toUpperCase();
       let goodInput = true;
       let otherTemperature = "";
 
+      // Based on temperature input type, calculate opposite type temperature
       if (temperatureType == "F") 
          otherTemperature = `${Math.floor((temperatureValue - 32) * .5556)}° C`;
       else if (temperatureType == "C") 
@@ -27,7 +28,7 @@
       else 
          goodInput = false;
       
-
+      // Show results. If bad input let user know.
       if (!goodInput)
          message.channel.send({ content: `I don't understand :joy:\nMake sure the temperature is formatted like these: 10C, 10F`});
       else
