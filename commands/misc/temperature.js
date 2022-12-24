8196a2d6329b8ee3b5052e0f8d26b3bd52768a3e
Daniel <edanielarredondo@gmail.com>
2022-12-24 01:44:37 -0600

    Bug

--- commands/misc/temperature.js
+++ commands/misc/temperature.js
@@ -23,7 +23,7 @@ module.exports = {
       else if (temperatureType == "C")
          otherTemperature = `${Math.floor((temperatureValue * 1.8) + 32)}° F`;
       else if (temperatureType == "K")
-         otherTemperature = `${Math.floor((temperatureValue - 273.15))}° C and ${Math.floor((1.8*(temperatureValue - 273) + 32))}° F`
+         otherTemperature = `${Math.floor((temperatureValue - 273.15))}° C and ${Math.floor((1.8 * (temperatureValue - 273) + 32))}° F`
 
       // Send message.
       message.channel.send({ content: `${temperatureValue}° ${temperatureType} is ${otherTemperature}` });
@@ -31,9 +31,9 @@ module.exports = {
       // Helper function
       function askForBetterInput() {
 
-         if (temperatureValue == "yourmom")
+         if (inputTemperature == "yourmom")
             message.channel.send({ content: `:weary::hot_face::weary::hot_face:` });
-         else 
+         else
             message.channel.send({ content: `I don't understand :joy:\nMake sure the temperature is formatted like these: 10C, 10F` });
       }
    },
