35aacd1dbd105aa872f59109eb603bdd2e9b43a1
Daniel <edanielarredondo@gmail.com>
2023-01-02 10:14:31 -0600

    add dice roll command

---
+++ commands/misc/diceRoll.js
@@ -0,0 +1,19 @@
+// Author @Mip
+
+module.exports = {
+	name: "random",
+
+	execute(message, args) {
+
+		// Get values and setup variables.
+      const diceSize = parseInt(args[0], 10);
+      
+      // Output
+      if (!Number.isInteger(diceSize)) {
+         message.channel.send({ content: `No decimals or letters please :(`});
+      } else {
+         const roll = Math.floor(Math.random() * (diceSize - 1 + 1) + 1)
+         message.channel.send({ content: `You rolled a ${roll}!`});
+      }
+	},
+};
