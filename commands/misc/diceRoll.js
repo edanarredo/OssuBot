b77c800333502b13da57d4fce1cb7223b4807b6e
Daniel <edanielarredondo@gmail.com>
2023-01-02 11:39:04 -0600

    Fix error message again

--- commands/misc/diceRoll.js
+++ commands/misc/diceRoll.js
@@ -9,8 +9,8 @@ module.exports = {
       const diceSize = parseInt(args[0], 10);
       
       // Output
-      if (!Number.isInteger(diceSize)) {
-         message.channel.send({ content: `Please enter a number that will be the dice size after !random. No decimals or letters please :(`});
+      if (!Number.isInteger(diceSize) || diceSize <= 0) {
+         message.channel.send({ content: `Please include the dice size in the command. No negative numbers, decimals, or letters please ;-;`});
       } else {
          const roll = Math.floor(Math.random() * (diceSize - 1 + 1) + 1)
          message.channel.send({ content: `You rolled a ${roll}!`});
